cd5e9e9e8ab0c4dd137b9a4052153c69723d5158
Prithvi Kashyap <prithvi.kashyap1804@gmail.com>
2024-09-24 10:36:51 +0530

    updates

--- Customer.cpp
+++ Customer.cpp
@@ -5,18 +5,37 @@ using namespace std;
 
 class Customer {
 public:
-    
     string name;
     int age;
-
     
-    void setDetails(string name, int age) {
-        this->name = name;  
-        this->age = age;   
+    static int totalCustomers;  // Static variable to count total number of customers
+
+    // Constructor
+    Customer() {
+        totalCustomers++;  // Increment totalCustomers when a new Customer object is created
     }
 
+    // Destructor
+    ~Customer() {
+        totalCustomers--;  // Decrement totalCustomers when a Customer object is destroyed
+    }
+
+    // Static member function to get total number of customers
+    static int getTotalCustomers() {
+        return totalCustomers;
+    }
+
+    // Member function to set details of a customer
+    void setDetails(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+
+    // Member function to display details of a customer
     void displayDetails() {
         cout << "Customer Name: " << this->name << ", Age: " << this->age << endl;
     }
-
 };
+
+// Initialize the static variable
+int Customer::totalCustomers = 0;
