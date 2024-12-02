56e9238580740cd542920ae4e486096d5d82c2d6
Prithvi Kashyap <prithvi.kashyap1804@gmail.com>
2024-12-02 11:05:18 +0530

    updates

--- Shoe.cpp
+++ Shoe.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
+// Base class for all shoes
 class Shoe {
 protected:
     string brand;
@@ -9,48 +11,49 @@ protected:
     int size;
 
 public:
-    Shoe() : brand("Unknown"), type("Unknown"), size(0) {}
     Shoe(string brand, string type, int size) : brand(brand), type(type), size(size) {}
 
-    void setDetails(string b, string t, int s) {
-        brand = b;
-        type = t;
-        size = s;
-    }
-
-    // Public getter methods for protected members
     string getBrand() const { return brand; }
     string getType() const { return type; }
     int getSize() const { return size; }
+
+    virtual void displayDetails() {
+        cout << "Brand: " << brand << ", Type: " << type << ", Size: " << size << endl;
+    }
+
+    virtual ~Shoe() {} // Virtual destructor to support polymorphism
 };
 
-// Separate class for displaying shoe details (SRP implementation)
+// Separate class for displaying shoe details (SRP applied)
 class ShoeDisplay {
 public:
     static void displayDetails(Shoe* shoe) {
-        cout << "Brand: " << shoe->getBrand() 
-             << ", Type: " << shoe->getType() 
-             << ", Size: " << shoe->getSize() << endl;
+        shoe->displayDetails();
     }
 };
 
-// Sneaker subclass inheriting Shoe
+// Derived classes
 class Sneaker : public Shoe {
 public:
-    Sneaker() : Shoe() {}
     Sneaker(string brand, int size) : Shoe(brand, "Sneaker", size) {}
 };
 
-// Boot subclass inheriting Shoe
 class Boot : public Shoe {
 public:
-    Boot() : Shoe() {}
     Boot(string brand, int size) : Shoe(brand, "Boot", size) {}
 };
 
-// Sandal subclass inheriting Shoe
 class Sandal : public Shoe {
 public:
-    Sandal() : Shoe() {}
     Sandal(string brand, int size) : Shoe(brand, "Sandal", size) {}
+};
+
+// using the ocp concept
+class Formal : public Shoe {
+public:
+    Formal(string brand, int size) : Shoe(brand, "Formal", size) {}
+
+    void displayDetails() override {
+        cout << "Formal Shoe - Brand: " << brand << ", Size: " << size << endl;
+    }
 };
